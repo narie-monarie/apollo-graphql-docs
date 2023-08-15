895b83a34113d9c0b5ea28badf81800ee884523a
job monari <jobmonari7@gmail.com>
2023-08-16 01:47:00 +0300

    related data

--- src/resolvers/resolvers.js
+++ src/resolvers/resolvers.js
@@ -11,10 +11,18 @@ export const resolvers = {
     review: (_, args) =>
       db.reviews.find((review) => review.id.toString() === args.id),
 
-    review: (_, args) =>
-      db.games.find((game) => game.id.toString() === args.id),
+    game: (_, args) => db.games.find((game) => game.id.toString() === args.id),
 
-    review: (_, args) =>
+    author: (_, args) =>
       db.authors.find((author) => author.id.toString() === args.id),
+  },
+
+  Game: {
+    reviews: (parent) =>
+      db.reviews.filter((review) => review.game_id.toString() === parent.id),
+  },
+  Author: {
+    reviews: (parent) =>
+      db.reviews.filter((review) => review.author_id.toString() === parent.id),
   },
 };
